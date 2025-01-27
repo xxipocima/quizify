673088fdb7375dcfe097bd1e0a8ea996842ff1e1
xxipocima <xipacima@gmail.com>
2025-01-27 10:54:09 +0200

    delete private

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,13 +2,5 @@ export const environment = {
   production: true,
   baseHref: '/quizify/#',
   firebase: {
-    apiKey: "AIzaSyBtcgM_2kDXHqCHmCGKCqNW-CCKUbUR1G8",
-    authDomain: "multiple-choice-question-7cd57.firebaseapp.com",
-    databaseURL: "https://multiple-choice-question-7cd57-default-rtdb.europe-west1.firebasedatabase.app",
-    projectId: "multiple-choice-question-7cd57",
-    storageBucket: "multiple-choice-question-7cd57.firebasestorage.app",
-    messagingSenderId: "525728890903",
-    appId: "1:525728890903:web:bab85dcf8dfe3faa131c52",
-    measurementId: "G-S4S315YQYP"
   }
 };
