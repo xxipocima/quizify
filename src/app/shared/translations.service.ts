cba2a6a3a775344c14f8004f8f2c616556362bcb
dsandul <dmitriy@sandul.pro>
2025-04-17 19:16:57 +0300

    multilang support added

--- src/app/shared/translations.service.ts
+++ src/app/shared/translations.service.ts
@@ -2,6 +2,7 @@ import {Router} from '@angular/router';
 import {Injectable} from '@angular/core';
 import {AngularFirestore} from '@angular/fire/compat/firestore';
 import { TranslateService } from '@ngx-translate/core';
+import { AuthService } from './auth/auth.service';
 
 @Injectable({
   providedIn: 'root'
@@ -11,14 +12,15 @@ export class TranslationsService {
   constructor(
     private readonly fireStore: AngularFirestore,
     private readonly translateService: TranslateService,
+    private readonly authService: AuthService,
     public router: Router
   ) {}
 
   async updateTranslations(data: any) : Promise<boolean> {
-    let lang: string = this.translateService.currentLang;
-    return this.fireStore.collection('translations').doc(lang).update(data).then(() =>{
+    let lang: string = this.authService.getCurrentLang();
+    return this.fireStore.collection('translations').doc(lang).set(data, { merge: true }).then(() => {
       return true;
-    }).catch(function (error) {
+    }).catch((error) => {
       console.log(error)
       return error;
     })
